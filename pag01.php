fac459d281ced962763d2dd88fe0aefcc00c25f5
sara <sarapereiraalcantara123@gmail.com>
2022-05-19 19:28:58 -0300

    atividade

---
+++ pag01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	Valor 1: 
+	<input type="number" name="valor1"><br><br>
+
+	Valor 2:
+	<input type="number" name="valor2"><br><br>
+
+</body>
+</html>
